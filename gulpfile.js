854ba5ad7ac42c139db72134c616ff5b481d8092
htmlpluscss <79198889134@ya.ru>
2021-10-25 10:52:50 +0300

    breadcrumbs

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ try {
 
 }
 
-const html = (files, since = {}) => {
+const html = (files, since = {}, folder = '') => {
 
 	return gulp.src(files, since)
 		.pipe(plumber())
@@ -73,13 +73,13 @@ const html = (files, since = {}) => {
 		}))
 		.pipe(w3cjs())
 		.pipe(w3cjs.reporter())
-		.pipe(gulp.dest('build'))
+		.pipe(gulp.dest('build' + folder))
 
 };
 
 gulp.task('html', () => html('src/**/index.html', {since: gulp.lastRun('html')}));
 gulp.task('html:touch', () => html('src/**/index.html'));
-gulp.task('html:docs', () => html('src/docs/**/index.html'));
+gulp.task('html:catalog', () => html('src/catalog/**/index.html', {}, '/catalog'));
 
 gulp.task('css', () => {
 
@@ -196,7 +196,7 @@ gulp.task('watch', () => {
 	gulp.watch('src/js/*.*', gulp.series('js'));
 	gulp.watch('src/css/*.*', gulp.series('css'));
 	gulp.watch('src/**/index.html', gulp.series('html'));
-//	gulp.watch(['src/docs/**','!src/docs/index.html'], gulp.series('html:docs'));
+	gulp.watch(['src/catalog/**','!src/catalog/index.html'], gulp.series('html:catalog'));
 	gulp.watch(['src/_include/**/*.html','src/template/**/*.html'], gulp.series('html:touch'));
 	gulp.watch(['src/**/*.*', '!src/**/*.{css,html,js}'], gulp.series('copy'));
 	gulp.watch('build/**/*.*', gulp.series('ftp'));
