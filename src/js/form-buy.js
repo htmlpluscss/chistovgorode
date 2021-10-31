f8523e0a1314fc1051187dd6f0f6d09056aaced8
htmlpluscss <79198889134@ya.ru>
2021-10-31 22:10:35 +0300

    Товар добавлен в корзину

--- src/js/form-buy.js
+++ src/js/form-buy.js
@@ -1,16 +1,24 @@
-( () => {
+( modalCart => {
+
+	if ( !modalCart ) {
+
+		return;
+
+	}
+
+	const modalCartTemplate = document.querySelector('#modal-cart-template').innerHTML;
 
 	document.addEventListener('submit', event => {
 
-		let form = event.target.closest('.form-buy');
+		const form = event.target.closest('.form-buy');
 
 		if (form) {
 
+			const btnSubmit = form.querySelector('.form-buy__submit');
+
+			btnSubmit.disabled = true;
+
 			event.preventDefault();
-
-			const btn = form.querySelector('.form__submit');
-
-			btn.disabled = true;
 
 			form.classList.add('is-loading');
 
@@ -18,13 +26,26 @@
 				method: 'POST',
 				body: new FormData(form)
 			})
-//			.then(response => response.json())
+			.then(response => response.json())
 			.then(result => {
 
 				console.log(result);
 
 				form.classList.remove('is-loading');
-				btn.disabled = false;
+
+				btnSubmit.disabled = false;
+
+				modalCart.innerHTML = Mustache.render(modalCartTemplate, result);
+
+				// показать модалку
+
+				const eventModalShow = new CustomEvent("modalShow", {
+					detail: {
+						selector: "cart"
+					}
+				});
+
+				window.modal.dispatchEvent(eventModalShow);
 
 			});
 
@@ -32,4 +53,112 @@
 
 	});
 
-})();
+	// update count
+
+	modalCart.addEventListener('change', () => {
+
+		modalCart.classList.add('is-loading');
+
+		fetch(modalCart.getAttribute('action'), {
+			method: 'POST',
+			body: new FormData(modalCart)
+		})
+		.then(response => response.json())
+		.then(result => {
+
+			console.log(result);
+
+			modalCart.classList.remove('is-loading');
+
+			if ( result.id == modalCart.elements.id.value ) {
+
+				// частичный
+
+				modalCart.querySelector('.modal-cart__count').value = result.count;
+				modalCart.querySelector('.modal-cart__total-text').textContent = result.totalText;
+
+			} else {
+
+				// полный рендер
+
+				modalCart.elements.id.value = result.id;
+
+				modalCart.querySelector('.modal-cart__img').href = result.link;
+				modalCart.querySelector('.modal-cart__img img').src = result.img;
+
+				modalCart.querySelector('.modal-cart__name').href = result.link;
+				modalCart.querySelector('.modal-cart__name').textContent = result.name;
+
+				modalCart.querySelector('.modal-cart__intro').textContent = result.description;
+
+				if ( result.priceOld && modalCart.querySelector('.price__old') ) {
+
+					modalCart.querySelector('.price__sale').textContent = result.price;
+					modalCart.querySelector('.price__old').textContent = result.priceOld;
+
+				} else {
+
+					modalCart.querySelector('.price').textContent = result.price;
+
+				}
+
+				modalCart.querySelector('.modal-cart__count').value = result.count;
+				modalCart.querySelector('.modal-cart__total-text').textContent = result.totalText;
+
+			}
+
+		});
+
+	});
+
+	modalCart.addEventListener('submit', event => {
+
+		event.preventDefault();
+
+		modalCart.dispatchEvent(new CustomEvent("change"));
+
+	});
+
+	modalCart.addEventListener('keyup', event => {
+
+		console.log(event.key);
+
+		if ( event.target.classList.contains('quantity__input') ) {
+
+			event.target.value = event.target.value.replace(/[\D]/g, '');
+
+			modalCart.dispatchEvent(new CustomEvent("change"));
+
+		}
+
+	});
+
+	modalCart.addEventListener('click', event => {
+
+		const input = modalCart.querySelector('.quantity__input');
+
+		if ( event.target === input ) {
+
+			input.setSelectionRange(0,9);
+
+		}
+
+		if ( event.target.closest('.quantity__btn--up') ) {
+
+			input.value = parseInt(input.value) + 1;
+
+			modalCart.dispatchEvent(new CustomEvent("change"));
+
+		}
+
+		if ( event.target.closest('.quantity__btn--down') ) {
+
+			input.value = parseInt(input.value) <= 1 ? 1 : parseInt(input.value) - 1;
+
+			modalCart.dispatchEvent(new CustomEvent("change"));
+
+		}
+
+	});
+
+})(document.querySelector('.modal-cart'));
